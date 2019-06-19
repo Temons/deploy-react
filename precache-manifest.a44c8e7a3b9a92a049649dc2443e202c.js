c1960c0fd1de07b24e77c9db328edbe60d678a78
Artsiom Trafimiuk <algatecby@gmail.com>
2019-06-19 13:25:00 +0300

    Updates

---
+++ precache-manifest.a44c8e7a3b9a92a049649dc2443e202c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3239a5fa710fb20d08450e53732977a4",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "38bdb29e41d5cc583890",
+    "url": "/deploy-react/static/css/main.5d1d2ab2.chunk.css"
+  },
+  {
+    "revision": "6078ceb7eb00a1a3dfc1",
+    "url": "/deploy-react/static/js/2.e319e905.chunk.js"
+  },
+  {
+    "revision": "38bdb29e41d5cc583890",
+    "url": "/deploy-react/static/js/main.959607b3.chunk.js"
+  },
+  {
+    "revision": "a70ab81c67defc9fa7dc",
+    "url": "/deploy-react/static/js/runtime~main.5de6419e.js"
+  }
+]);
